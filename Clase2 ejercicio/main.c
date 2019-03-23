c14cb5cd4364ff83c8cf087e65abb7335bc3ebe8
NicolasPantano <nicolaspantano_757@hotmail.com>
2019-03-23 15:11:52 -0300

    Validacion y cantidad de pares

--- Clase2 ejercicio/main.c
+++ Clase2 ejercicio/main.c
@@ -6,11 +6,48 @@ int main()
 {
     int numero;
     char respuesta;
+    int cantNumeros;
+    int cantPares;
+    int sumaPositivos;
+    int cantNegativos;
+    int sumaNegativos;
+    float promedioNegativos;
+    int maximo;
+    int minimo;
+    int minimoPositivos;
+    int maximoNegativo;
+    int cantEnRangoDado;
+    int promedioTotal;
+
+
 
     do{
         printf("Ingrese un numero: ");
         scanf("%d",&numero);
+            while(numero==0)
+                {
+                    printf("El numero no puede ser 0. Ingrese un numero");
+                    scanf("%d",&numero);
+                }
         fflush(stdin);
+            if(numero%2==0)
+            {
+                cantPares++;
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+            cantNumeros++;
         printf("Desea continuar?");
         scanf("%c",&respuesta);
         fflush(stdin);
