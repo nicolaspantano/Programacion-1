017697e34e38d51a4c0a733da6274779e4f120ca
NicolasPantano <nicolaspantano_757@hotmail.com>
2019-03-26 10:27:52 -0300

    Clase 4 Recurisividad y bibliotecas

---
+++ Clase 4/Clase 4/Ingresos.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "Ingresos.h"
+
+
+int pedirEntero(char texto[]){
+int unEntero;
+printf("%s",texto);
+scanf("%d",&unEntero);
+return unEntero;
+
+}
+
---
+++ Clase 4/Clase 4/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Recursividad.h"
+#include "Ingresos.h"
+
+int main()
+{
+   int resultado=factorial(5);
+   printf("%d",resultado);
+
+    return 0;
+}
