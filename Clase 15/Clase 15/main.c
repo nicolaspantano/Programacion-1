41bd49b59856e216c6480804d37128afa716ded1
nicolaspantano <nicolaspantano_757@hotmail.com>
2019-05-21 12:32:36 -0300

    asd

--- Clase 15/Clase 15/main.c
+++ Clase 15/Clase 15/main.c
@@ -1,30 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define T 5
 int main()
 {
     int* pNumeros;
+    int* auxpInt;
     int i;
-    pNumeros=malloc(sizeof(int)*5);
+
+    pNumeros=malloc(sizeof(int)*T);
 
     if(pNumeros!=NULL)
     {
-        for(i=0;i<5;i++)
+        for(i=0; i<T; i++)
         {
-          //*(pNumeros+i)=i+1;
-            printf("Ingrese un numero");
+            //*(pNumeros+i)=i+1;
+            printf("Ingrese un numero: ");
             scanf("%d",pNumeros+i);
         }
-        for(i=0;i<5;i++)
+        for(i=0; i<T; i++)
         {
             printf("%d\n",*(pNumeros+i));
         }
 
     }
 
+  auxpInt=pNumeros;
+
+  if(auxpInt=realloc(auxpInt,sizeof(int)*(T+5)!=NULL))
+     {
+         pNumeros=realloc(pNumeros,sizeof(int)*(T+5));
+     }
+     else
+    {
+        printf("No hay espacio");
+
+    }
 
 
-
+    if(pNumeros!=NULL)
+    {
+        for(i=T; i<T+5; i++)
+        {
+            //*(pNumeros+i)=i+1;
+            printf("Ingrese un numero: ");
+            scanf("%d",pNumeros+i);
+        }
+        for(i=0; i<T+5; i++)
+        {
+            printf("%d\n",*(pNumeros+i));
+        }
+    }
 
 
 
